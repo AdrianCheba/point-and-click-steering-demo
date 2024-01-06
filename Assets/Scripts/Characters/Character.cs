ef006ed35718fa8817fc6dac947ac50bc9f13db3
Adrian Cheba <adiche@wp.pl>
2024-01-06 22:57:50 +0100

    Added character movement system
    The Character.cs script has been assigned to the Character prefab, which is used to control the character's movement. In the MoveTo() method we check by Raycast where on the plane the leader should go. This function checks which character is the leader and the rest are assigned the leader's Transform as a destination

--- Assets/Scripts/Characters/Character.cs
+++ Assets/Scripts/Characters/Character.cs
@@ -1,18 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.InputSystem;
 
 public class Character : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    CharacterConfig _charactersConfig;
+
+    [SerializeField]
+    int _characterID;
+
+    NavMeshAgent _navMeshAgent;
+
+    [SerializeField]
+    InputActionAsset _actionAsset;
+
+    void OnEnable()
     {
-        
+        _actionAsset.Enable();
+        _charactersConfig.LiderID = 0;
+        _charactersConfig.LiderTransform = null;
+        _navMeshAgent = gameObject.GetComponent<NavMeshAgent>();
+        _charactersConfig.MainCamera = Camera.main;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        MoveTo();
+    }
+
+    void MoveTo()
+    {
+        _actionAsset.FindAction("MouseClick").performed += _ =>
+        {
+            if (_charactersConfig.LiderID == 0)
+            {
+                Debug.Log("Select Character");
+                return;
+            }
+
+            Ray ray = _charactersConfig.MainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+            if (Physics.Raycast(ray: ray, hitInfo: out RaycastHit hit) && hit.collider)
+            {
+                if (_characterID == _charactersConfig.LiderID)
+                {
+                    _charactersConfig.LiderTransform = transform;
+                    _navMeshAgent.destination = hit.point;
+
+                }
+            }
+        };
+
+        if (_characterID != _charactersConfig.LiderID && _charactersConfig.LiderTransform != null)
+        {
+            _navMeshAgent.destination = _charactersConfig.LiderTransform.position;
+        }
     }
 }
